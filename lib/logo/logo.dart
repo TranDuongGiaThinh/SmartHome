ea754dcd72c23630f102e637e8e4658132a8c928
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-27 23:31:24 +0700

    add component logo

---
+++ lib/logo/logo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home/constants.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: MyConstants.appBackground,
+      child: Image.asset("data/logo.png", fit: BoxFit.fill),
+    );
+  }
+}
