52527b9dce2580665317d07183d1f363aed3bfc4
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-27 21:39:20 +0700

    acc componet Button

---
+++ lib/home/custom_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home/constants.dart';
+import 'package:smart_home/home/device_presenter.dart';
+import 'package:smart_home/models/device.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key, required this.device, required this.presenter});
+  final Device device;
+  final DevicePresenter presenter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: MyConstants.colorButton),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  presenter.getIconOfDevice(device),
+                  color: device.isOn
+                      ? MyConstants.colorIconLightOn
+                      : MyConstants.colorIconLightOff,
+                ),
+                Text(
+                  device.name,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: MyConstants.fontHeader,
+                      color: MyConstants.colorText),
+                ),
+                Switch(
+                    value: device.isOn,
+                    onChanged: (newValue) {
+                      presenter.onChangeValue(device);
+                    })
+              ],
+            ),
+          ]),
+    );
+  }
+}
