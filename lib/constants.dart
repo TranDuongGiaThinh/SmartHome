0019af5a3f7498bc922751f42761b4294a252eb4
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-27 21:38:58 +0700

    add file constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,32 @@
+import 'package:flutter/material.dart';
+
 class MyConstants{
+  static const double fontText = 13.0;
+  static const double fontHeader = 16.0;
+
+  static const Color colorText = Colors.black54;
+  static const Color colorTextWithBackground = Colors.white;
+
+  static const Color colorIconSelected = Colors.blue;
+  static const Color colorIconUnselected = Colors.black54;
+  
+  static const Color colorIconLightOn = Colors.yellowAccent;
+  static const Color colorIconLightOff = Colors.black54;
+
+  static const Color colorAppbar = Colors.green;
+  static const Color colorButton = Colors.greenAccent;
+
   static const int homeIndex = 0;
   static const int notiIndex = 1;
 
   static const String home = "Trang chủ";
   static const String noti = "Thông báo";
+  
+  static const int lightIndex = 0;
+  static const int fanIndex = 1;
+  static const int tiviIndex = 2;
+
+  static const String light = "Đèn";
+  static const String fan = "Quạt";
+  static const String tivi = "Tivi";
 }
