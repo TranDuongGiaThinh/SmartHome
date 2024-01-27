e880f575a0cdbb13efde87edc48274ace911d576
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-27 23:31:01 +0700

    add backgound

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MyConstants{
+class MyConstants {
   static const double fontText = 13.0;
   static const double fontHeader = 16.0;
+  
+  static const Color appBackground = Colors.greenAccent;
 
   static const Color colorText = Colors.black54;
   static const Color colorTextWithBackground = Colors.white;
+  static const Color colorTextInSignUp = Colors.green;
 
-  static const Color colorIconSelected = Colors.blue;
-  static const Color colorIconUnselected = Colors.black54;
-  
+  static const Color colorContentSelected = Colors.green;
+  static const Color colorContentUnselected = Colors.black54;
+
   static const Color colorIconLightOn = Colors.yellowAccent;
   static const Color colorIconLightOff = Colors.black54;
 
@@ -19,14 +22,23 @@ class MyConstants{
   static const int homeIndex = 0;
   static const int notiIndex = 1;
 
-  static const String home = "Trang chủ";
-  static const String noti = "Thông báo";
-  
   static const int lightIndex = 0;
   static const int fanIndex = 1;
   static const int tiviIndex = 2;
 
+  static const String appName = "AppName";
+  static const String hello = "Xin Chào!";
+  static const String wellcome = "Chào mừng bạn đến với $appName";
+
+  static const String home = "Trang chủ";
+  static const String noti = "Thông báo";
+
   static const String light = "Đèn";
   static const String fan = "Quạt";
   static const String tivi = "Tivi";
+
+  static const String on = "Đang bật";
+
+  static const String enterName = "Nhập tên của bạn";
+  static const String ok = "OK";
 }
