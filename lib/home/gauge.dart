c66caa7ade96bb768199d3befcd52ebd47b7e1f8
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-28 23:17:58 +0700

    add guage

---
+++ lib/home/gauge.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home/constants.dart';
+import 'package:smart_home/home/device_presenter.dart';
+
+class Guage extends StatelessWidget {
+  const Guage({
+    super.key,
+    required this.name,
+    required this.value,
+    required this.icon,
+    required this.unit,
+    required this.height,
+    required this.presenter,
+  });
+  final String name;
+  final int value;
+  final IconData icon;
+  final String unit;
+  final double height;
+  final DevicePresenter presenter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          height: height,
+          width: height,
+          decoration: BoxDecoration(
+            color: presenter.getColorUsageRate(name),
+            shape: BoxShape.circle,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: height / 3,
+                color: MyConstants.colorIcon,
+              ),
+              Text(
+                "$value $unit",
+                style: TextStyle(
+                  fontSize: name == MyConstants.guageUsage
+                      ? MyConstants.fontHeader * 2.5
+                      : MyConstants.fontHeader,
+                  color: MyConstants.colorText,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+        Text(
+          name,
+          style: const TextStyle(
+            fontSize: MyConstants.fontHeader,
+            color: MyConstants.colorText,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
+    );
+  }
+}
