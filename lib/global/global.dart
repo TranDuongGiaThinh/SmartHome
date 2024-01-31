3832e8b38761070dbebcd4bd253011d2ae64c571
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-01-31 11:45:28 +0700

    separate files

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart';
+import '../data/repositories/data_local_repository.dart';
 
 class GlobalData {
   static String username = "";
@@ -33,83 +31,3 @@ class GlobalData {
       {"username": username, "boardname": boardname};
 }
 
-class DataLocal {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-
-    File file = File('$path/data.json');
-    if (!(await file.exists())) {
-      await file.create(recursive: true);
-    }
-    return file;
-  }
-
-  Future<String> getStringJson() async {
-    String s = "";
-    try {
-      final localFile = await _localFile;
-      s = await localFile.readAsString();
-      if (kDebugMode) {
-        print("getStringJson() thành công");
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print("getStringJson() thất bại: $e");
-      }
-    }
-    return s;
-  }
-
-  Future<Map<String, dynamic>> getMap() async {
-    try {
-      Map<String, dynamic> map = jsonDecode(await getStringJson());
-      if (kDebugMode) {
-        print("getMap() từ local thành công");
-      }
-      return map;
-    } catch (e) {
-      if (kDebugMode) {
-        print("getMap() từ local thất bại: $e");
-      }
-      return {};
-    }
-  }
-
-  Future<void> save(Map<String, dynamic> map) async {
-    try {
-      final file = await _localFile;
-
-      final jsonString = json.encode(map);
-
-      await file.writeAsString(jsonString);
-      if (kDebugMode) {
-        print("Lưu dữ liệu ở local thành công");
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print("Lưu dữ liệu ở local thất bại: $e");
-      }
-    }
-  }
-
-  Future<void> clear() async {
-    try {
-      final file = await _localFile;
-
-      await file.writeAsString('');
-
-      if (kDebugMode) {
-        print("Xóa dữ liệu ở local thành công");
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print("Xóa dữ liệu ở local thất bại: $e");
-      }
-    }
-  }
-}
